805f5a846738a8c3e91bc59a4b0fda02401eae28
JordynWinnie <jordynwinnie@gmail.com>
2020-11-17 12:50:00 +0800

    Changed how Profiles Work
    - Game Manager AutoLoads Profiles now

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private List<Profile> profiles;
+    [SerializeField] private Profile[] profiles;
     public static GameManager instance;
     public Profile currentProfile;
 
     private void Awake()
     {
+        profiles = Resources.LoadAll<Profile>("Profiles");
+
         if (instance == null)
         {
             instance = this;
@@ -23,7 +25,7 @@ public class GameManager : MonoBehaviour
 
     public Profile ReturnRandomProfile()
     {
-        currentProfile = profiles[Random.Range(0, profiles.Count)];
+        currentProfile = profiles[Random.Range(0, profiles.Length)];
         print(currentProfile.profileName);
         return currentProfile;
     }
@@ -31,5 +33,13 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+    }
+
+    private void Start()
+    {
+        foreach (var profile in profiles)
+        {
+            print(profile.profileName);
+        }
     }
 }
