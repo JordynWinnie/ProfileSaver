d4aea91f600268018a957eed4a571b6dd84f3610
JordynWinnie <jordynwinnie@gmail.com>
2020-11-18 00:14:20 +0800

    Game Systems Implemented
    Game manager can now change money, health, happiness and time

---
+++ Assets/DisplayInformation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayInformation : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timeUI;
+    [SerializeField] private TextMeshProUGUI moneyUI;
+    [SerializeField] private TextMeshProUGUI healthUI;
+    [SerializeField] private TextMeshProUGUI happinessUI;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeUI.text = "Time: " + GameManager.instance.ReturnTimeString();
+        moneyUI.text = "Money: $" + GameManager.instance.Money.ToString();
+        healthUI.text = "Health: " + GameManager.instance.Health.ToString();
+        happinessUI.text = "Happiness: " + GameManager.instance.Happiness.ToString();
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,38 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     public Profile currentProfile;
 
+    private float _health = 10f;
+    private float _happiness = 10f;
+    private float _money = 1000f;
+
+    public float Health
+    {
+        get => _health;
+        set
+        {
+            if (value <= 10)
+            {
+                _health = value;
+            }
+        }
+    }
+
+    public float Happiness
+    {
+        get => _happiness;
+        set
+        {
+            if (value <= 10)
+            {
+                _happiness = value;
+            }
+        }
+    }
+
+    public float Money { get => _money; set => _money = value; }
+
+    private float timeInHours = 0f;
+
     private void Awake()
     {
         profiles = Resources.LoadAll<Profile>("Profiles");
@@ -26,20 +58,58 @@ public class GameManager : MonoBehaviour
     public Profile ReturnRandomProfile()
     {
         currentProfile = profiles[Random.Range(0, profiles.Length)];
-        print(currentProfile.profileName);
         return currentProfile;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            AddTime(0.5f);
+            print(ReturnTimeString());
+        }
     }
 
     private void Start()
     {
-        foreach (var profile in profiles)
+    }
+
+    public float ReturnTime()
+    {
+        return timeInHours;
+    }
+
+    public float AddTime(float timeToIncrease)
+    {
+        timeInHours += timeToIncrease;
+        return timeInHours;
+    }
+
+    public int ReturnDayNumber()
+    {
+        return (int)timeInHours / 24 + 1;
+    }
+
+    public int ReturnHour()
+    {
+        return (int)timeInHours % 24;
+    }
+
+    public float ReturnMinutes()
+    {
+        return (timeInHours % 24 % 1) * 60;
+    }
+
+    public string ReturnTimeString()
+    {
+        if (ReturnHour() < 12)
         {
-            print(profile.profileName);
+            return $"{ReturnHour().ToString().PadLeft(2, '0')}:{ReturnMinutes().ToString().PadLeft(2, '0')} AM DAY {ReturnDayNumber()}";
+        }
+        else
+        {
+            return $"{ReturnHour().ToString().PadLeft(2, '0')}:{ReturnMinutes().ToString().PadLeft(2, '0')} PM DAY {ReturnDayNumber()}";
         }
     }
 }
