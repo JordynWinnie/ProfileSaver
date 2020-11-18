10cfeaa2c55f791b20bb34e9f056ad3762d07c18
Jordyn <48559311+JordynWinnie@users.noreply.github.com>
2020-11-18 11:28:14 +0800

    Choices and Decision Implemented

---
+++ Assets/Scripts/Decision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+[CreateAssetMenu(fileName = "Data", menuName = "Make New Decision/Decision", order = 2)]
+public class Decision : ScriptableObject
+{
+    public string decisionTitle;
+    public string decisionString;
+    public Sprite decisionIcon;
+    public List<Choices> availableChoices;
+
+}
