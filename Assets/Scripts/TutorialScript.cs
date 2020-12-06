d6b236d831f99c10fd35f1a8dd627717e3eb98a3
Asscreek <74607313+Asscreek@users.noreply.github.com>
2020-12-06 21:26:42 +0800

    removed fullstop

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -39,7 +39,7 @@ public class TutorialScript : MonoBehaviour
             new Tutorial("Start off the game by choosing a profile. You can shuffle through profiles to choose who you want to play as.", tutorialImages[0]),
             new Tutorial("Your character has hunger, health, happiness, energy and money stats. Goal of the game is to maintain all these statistics while managing your money. Have to play the game according to the character profile you have chosen.", tutorialImages[1]),
             new Tutorial("Each location has different choices that you can choose. Some locations are only open at certain times.", tutorialImages[2]),
-            new Tutorial("Don't take too long to decide as there is a timer which will run out and there will be penalties! Travelling also uses up hunger and time, so spend your time wisely!.", tutorialImages[3]),
+            new Tutorial("Don't take too long to decide as there is a timer which will run out and there will be penalties! Travelling also uses up hunger and time, so spend your time wisely!", tutorialImages[3]),
             new Tutorial("Random events might appear as you play the game.", tutorialImages[4]),
             new Tutorial("There are two types of goals - daily and monthly. strive to accomplish the goals - daily for rewards, monthly for better star rating at the end of the month!", tutorialImages[5]),
             new Tutorial("After about a month (avg 30 days passed), your stats will be calculated and star ratings will be given to you.", tutorialImages[6]),
